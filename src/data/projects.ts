f6bed321b549479139e3ebb51f53c7da26151359
rohanbennur43 <rohan.bennur@gmail.com>
2026-02-12 17:08:57 -0800

    Update with new project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -21,8 +21,17 @@ export const projects: Project[] = [
     additionalLink: "https://star.cs.ucr.edu/", // Added official website link
     tags: ["Python","Memory Management", "Geospatial Visualization", "R* Tree", "Z-order Curve"],
   },
-    {
+  {
     id: "project-9",
+    title: "ProtoForgeAI",
+    description:
+      "ProtoForgeAI generates real, compilable Go gRPC tests directly from your .proto files. It automates repetitive gRPC test scaffolding while ensuring correctness, determinism, and engineering control. The tool parses your schema using protoc, extracts services, messages, fields, and enums, and generates tests strictly constrained to that structure. The generated code is validated with go build, and any compiler errors are automatically repaired in a bounded loop.",
+    demoUrl: "https://github.com/rohanbennur43/ProtoForgeAI",
+    githubUrl: "https://github.com/rohanbennur43/ProtoForgeAI",
+    tags: ["Go", "gRPC", "Protobuf", "RAG's", "AI Assisted Code Generation", "OpenAI API", "LangChain", "Ollama API"],
+  },
+  {
+    id: "project-10",
     title: "B-tree",
     description:
       "An implementation of the B-tree data structure designed for efficient indexing and disk based storage. The project demonstrates balanced tree operations including insertion, deletion, and search while maintaining logarithmic performance. Built to highlight core database indexing concepts and optimized memory management techniques.",
